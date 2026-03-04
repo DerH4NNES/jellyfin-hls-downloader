ae0776b55ed0becd6bd64f89126f5c7e12328bf4
Sascha Hannes <hannes@publicplan.de>
2026-03-04 21:44:27 +0100

    init project

---
+++ Jellyfin.Plugin.HLSDownloader/Configuration/PluginConfiguration.cs
@@ -0,0 +1,15 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.HLSDownloader.Configuration;
+
+/// <summary>
+/// Plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Gets or sets the downloadDistPath (lokaler Pfad auf Jellyfin-System).
+    /// </summary>
+    [DefaultValue("")]
+    public string DownloadDistPath { get; set; } = "";
+}
